ed06269332908308573c3df612cd6cbea8fc4d13
RafaelEduardoDiaz <rafael.diaz@usantotomas.edu.co>
2020-09-07 13:19:44 -0500

    Se organiza el codigo en Query_osmdata.R

--- Query_osmdata.R
+++ Query_osmdata.R
@@ -6,38 +6,50 @@ library(sf)
 library(geojsonsf)
 library(mapview)
 
-#
+## Caracteristicas disponibles
 available_features()
 
-#amenities
+## Etiquetas dentro de una caracteristica
 available_tags("amenity")
 
+## ------------------------------------------------------ ##
+## ---- Ejercicio 1 - bancos poligono parkway Bogotá ---- ##
+## ------------------------------------------------------ ##
 
-#my_box <- c(4.6304414673187,-74.075607061386,4.6332058140013,-74.072549343109);
+## Defino una caja
 my_box <- c(-74.075607061386, 4.6304414673187, -74.072549343109, 4.6332058140013)
-bank_pol <- opq(bbox = my_box, timeout = 25*100) %>% 
-  add_osm_feature(key = "amenity", "bank") %>% 
-  osmdata_sf()
+bank_pol <- opq(bbox = my_box, timeout = 25*100) %>% add_osm_feature(key = "amenity", "bank") %>% osmdata_sf()
 
-
+## Gráfico los bancos mapeados como nodos - puntos
 mapview(subset(bank_pol$osm_points[,c("osm_id","name","amenity","atm")], amenity == "bank"), map.types = "OpenStreetMap")
+
+## Gráfico los bancos mapeados como vías - poligonos
 mapview(bank_pol$osm_polygons[,c("osm_id","name","amenity","atm","operator")], map.types = "OpenStreetMap")
 
+## Uno nodos y vías
 res1 <- subset(bank_pol$osm_points[,c("osm_id","name","amenity","atm")], amenity == "bank")
 res2 <- mutate(bank_pol$osm_polygons[,c("osm_id","name","amenity","atm","operator")], name = ifelse(is.na(name), operator, name)) %>% 
   select(-"operator") %>% mutate(geometry = st_centroid(geometry), id = osm_id) %>% remove_rownames %>% column_to_rownames(var = "id")
 
+## Gráfico la totalidad de los bancos
 mapview(bind_rows(res1, res2))
 
-bank_pol
+## ------------------------------------------------------ ##
+## ---- Ejercicio 2 - Total de bancos en Bogotá      ---- ##
+## ------------------------------------------------------ ##
 
+## Extraigo la caja de Bogota
 bank_bog <- opq(bbox = getbb(place_name = "Bogota", display_name_contains = "Colombia"), timeout = 25*100) %>% 
   add_osm_feature(key = "amenity", "bank") %>% 
   osmdata_sf()
 
-mapview(bank_bog$osm_polygons[,c("osm_id","name","amenity","atm","operator")])
+## Gráfico los bancos mapeados como nodos - puntos
 mapview(subset(bank_bog$osm_points[,c("osm_id","name","amenity","atm")], amenity == "bank"))
 
+## Gráfico los bancos mapeados como vías - poligonos
+mapview(bank_bog$osm_polygons[,c("osm_id","name","amenity","atm","operator")])
+
+## Gráfico los bancos mapeados como vías - poligonos
 res_bank <- bind_rows(subset(bank_bog$osm_points[,c("osm_id","name","amenity","atm")], amenity == "bank"),
                       mutate(bank_bog$osm_polygons[,c("osm_id","name","amenity","atm","operator")], name = ifelse(is.na(name), operator, name)) %>% 
                         select(-"operator") %>% mutate(geometry = st_centroid(geometry), id = osm_id) %>% remove_rownames %>% column_to_rownames(var = "id"))
@@ -47,18 +59,18 @@ as.tibble(res_bank) %>% group_by(name) %>% summarise(Total = n()) %>% arrange(de
 
 subset(res_bank,name == "Servintrega")
 
-##################
-available_features()
-available_tags("boundary")
+## ------------------------------------------------------ ##
+## ---- Ejercicio 3 - Gráfico la cartografía de Bogotá -- ##
+## ------------------------------------------------------ ##
 
-
-bog <- opq(bbox = c(-74.192733764648,4.5120831618535,-74.009399414062,4.778994921541), timeout = 25*100) %>% 
-  add_osm_feature(key = "boundary", "administrative") %>% 
-  osmdata_sf()
-
+bog <- opq(bbox = "Bogota") %>% add_osm_feature(key = "boundary", "administrative") %>% osmdata_sf()
 mapview(bog$osm_lines)
 
 
-callesMan <- opq(bbox = c(-74.192733764648,4.5120831618535,-74.009399414062,4.778994921541)) %>% 
+## ------------------------------------------------------ ##
+## ---- Ejercicio 4 - Query algunas calles de Bogotá ---- ##
+## ------------------------------------------------------ ##
+
+callesMan <- opq(bbox = "Bogota") %>% 
   add_osm_feature(key = "highway", value = c("residential", "living_street","unclassified","service", "footway")) %>% osmdata_sf()
 mapview(callesMan$osm_lines)
