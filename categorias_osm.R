6a7b3ff9ebb84ee6d4b1c30ae85e0106a500b30e
RafaelEduardoDiaz <rafael.diaz@usantotomas.edu.co>
2020-09-15 00:00:41 -0500

    Se corrige la base Final_Todos_OSM.RDS

--- categorias_osm.R
+++ categorias_osm.R
@@ -290,3 +290,5 @@ Final_Todos <- as.data.table(readRDS("Bases/Final_Todos.RDS"))
 
 Final_Todos_OSM <- left_join(x = Final_Todos, y = osm_dcast, by = c("ID_Poly_100"="ID"))
 saveRDS(Final_Todos_OSM, "Bases/Final_Todos_OSM.RDS")
+
+Final_Todos_OSM <- readRDS("Bases/Final_Todos_OSM.RDS")
